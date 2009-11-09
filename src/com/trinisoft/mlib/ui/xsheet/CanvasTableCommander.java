b2919e0f87aa080c8a25f0c8c48ceedf34928c10
Akintayo A. Olusegun <trinisoftinc@hannamontana-2.local>
2009-11-09 14:47:30 +0100

    Insert new row working

--- src/com/trinisoft/mlib/ui/xsheet/CanvasTableCommander.java
+++ src/com/trinisoft/mlib/ui/xsheet/CanvasTableCommander.java
@@ -4,6 +4,7 @@
  */
 package com.trinisoft.mlib.ui.xsheet;
 
+import java.io.DataInput;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
@@ -97,6 +98,45 @@ public class CanvasTableCommander implements CommandListener {
 
             for (int i = 0; i < canvasTable.numCol; i++) {
                 canvasTable.data[canvasTable.numRow - 1][i] = "";
+            }
+
+            canvasTable.repaint();
+        }
+
+        if(c.getLabel().equals("Add Column")) {
+            String[][] temp = canvasTable.data;
+            canvasTable.numCol++;
+            canvasTable.data = new String[canvasTable.numRow][canvasTable.numCol];
+            for (int i = 0; i < temp.length; i++) {
+                for (int j = 0; j < temp[i].length; j++) {
+                    canvasTable.data[i][j] = temp[i][j];
+                }
+            }
+            for (int i = 0; i < canvasTable.numRow; i++) {
+                canvasTable.data[i][canvasTable.numCol] = "";
+            }
+
+            canvasTable.repaint();
+        }
+
+        if(c.getLabel().equals("Insert Row")) {
+            String[][] temp = canvasTable.data;
+            int currentRow = canvasTable.currentRow - 1;
+            canvasTable.numRow++;
+            canvasTable.data = new String[canvasTable.numRow][canvasTable.numCol];
+
+            for(int i = 0; i < canvasTable.numRow; i++) {
+                for(int j = 0; j < canvasTable.numCol; j++) {
+                    if(i == currentRow) {
+                        for(int k = 0; k < temp[i].length; k++) {
+                            canvasTable.data[i][k] = "";
+                        }
+                    } else if(i > currentRow) {
+                        canvasTable.data[i][j] = temp[i - 1][j];
+                    } else if(i < currentRow) {
+                        canvasTable.data[i][j] = temp[i][j];
+                    }
+                }
             }
 
             canvasTable.repaint();
