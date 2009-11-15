8e24a5f2c38b7b8f1c5d7dd0a93ff1356d868de3
Akintayo A. Olusegun <trinisoftinc@hannamontana-2.local>
2009-11-15 06:11:11 +0100

    Added overloaded constructors to TickerForm to enable it be a controllable form

--- src/com/trinisoft/mlib/ui/TickerForm.java
+++ src/com/trinisoft/mlib/ui/TickerForm.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.trinisoft.mlib.ui;
 
+import com.trinisoft.mlib.util.Controller;
 import java.util.TimerTask;
 import javax.microedition.lcdui.Ticker;
 import javax.microedition.rms.RecordStore;
@@ -14,6 +14,18 @@ import javax.microedition.rms.RecordStore;
  * @author segun
  */
 public class TickerForm extends Form {
+
+    public TickerForm(String title, String initialText, String tickerURL, Controller controller) {
+        super(title, controller);
+
+        this.setTicker(new Ticker(initialText));
+    }
+
+    public TickerForm(String title, String initialText, RecordStore store, Controller controller) {
+        super(title, controller);
+
+        this.setTicker(new Ticker(initialText));
+    }
 
     public TickerForm(String title, String initialText, String tickerURL) {
         super(title, null);
@@ -26,7 +38,6 @@ public class TickerForm extends Form {
 
         this.setTicker(new Ticker(initialText));
     }
-    
 }
 
 class TickerPull extends TimerTask {
@@ -34,9 +45,9 @@ class TickerPull extends TimerTask {
     public TickerPull(String url) {
     }
 
-    public TickerPull(RecordStore recordStore) {        
+    public TickerPull(RecordStore recordStore) {
     }
-    
-    public void run() {       
-    }    
+
+    public void run() {
+    }
 }
