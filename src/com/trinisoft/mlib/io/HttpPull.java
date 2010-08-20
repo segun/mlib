57993b7e24a2614ff005df49512f4a90db144847
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2010-08-20 09:05:15 +0100

    HTTPPULL bug resolved.

--- src/com/trinisoft/mlib/io/HttpPull.java
+++ src/com/trinisoft/mlib/io/HttpPull.java
@@ -4,6 +4,7 @@
  */
 package com.trinisoft.mlib.io;
 
+import com.trinisoft.mlib.login.db.UserClass;
 import com.trinisoft.mlib.util.Echo;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,15 +17,34 @@ import javax.microedition.io.HttpConnection;
  * @author trinisoftinc
  */
 public class HttpPull {
+    private String userAgent = "Profile/MIDP-2.1 Confirguration/CLDC-1.1";
+    private String APIKey = "";
 
     public HttpPull() {
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public String getAPIKey() {
+        return APIKey;
+    }
+
+    public void setAPIKey(String APIKey) {
+        this.APIKey = APIKey;
     }
 
     public String get(String url) throws IOException {
         HttpConnection connection = (HttpConnection) Connector.open(url);
 
         connection.setRequestMethod(HttpConnection.GET);
-        connection.setRequestProperty("User-Agent", "Profile/MIDP-2.1 Confirguration/CLDC-1.1");
+        connection.setRequestProperty("User-Agent", userAgent);
+        connection.setRequestProperty("api-key", APIKey);
         int rc = connection.getResponseCode();
         if (rc == HttpConnection.HTTP_OK) {
             InputStream is = connection.openInputStream();
