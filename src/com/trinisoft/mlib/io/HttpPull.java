70b787cc00b3c1567bcac9253b994976a30c3fb7
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2011-04-20 14:48:19 +0100

    We should change the way we handle BB connection...each app should handle their connection

--- src/com/trinisoft/mlib/io/HttpPull.java
+++ src/com/trinisoft/mlib/io/HttpPull.java
@@ -44,6 +44,7 @@ public class HttpPull {
         }
         HttpConnection connection = (HttpConnection) Connector.open(url);
 
+        Echo.outln(url);
         connection.setRequestMethod(HttpConnection.GET);
         connection.setRequestProperty("User-Agent", userAgent);
         connection.setRequestProperty("api-key", APIKey);
--- src/com/trinisoft/mlib/util/URLConstants.java
+++ src/com/trinisoft/mlib/util/URLConstants.java
@@ -10,25 +10,12 @@ package com.trinisoft.mlib.util;
  */
 public class URLConstants {
     public static String platform = System.getProperty("microedition.platform");
-    public static int isBB = platform.indexOf("BlackBerry");
-    public static String getAPN(String network) {        
-        //Echo.outln("ISBB: " + (isBB >= 0));
-        if (isBB < 0) {            
-            return "";
-        } else if (network.equals("Zain")) {
-            return ZAIN_APN;
-        } else if (network.equals("MTN")) {
-            return MTN_APN;
-        } else if (network.equals("GLO")) {
-            return GLO_APN;
-        } else return null;
-    }
-    public static final String USE_DEVICE_SIDE_STRING = ";deviceside=true";
-    public static String MTN_APN = USE_DEVICE_SIDE_STRING + ";apn=web.gprs.mtnnigeria.net;"
+    
+    public static String MTN_APN = ";apn=web.gprs.mtnnigeria.net;"
             + "tunnelauthusername=web;tunnelauthpassword=web";
-    public static String ZAIN_APN = USE_DEVICE_SIDE_STRING + ";apn=internet.ng.zain.com;"
+    public static String ZAIN_APN = ";apn=internet.ng.zain.com;"
             + "tunnelauthusername=web;tunnelauthpassword=web";
-    public static String GLO_APN = USE_DEVICE_SIDE_STRING + ";apn=glowap;"
+    public static String GLO_APN = ";apn=glowap;"
             + "tunnelauthusername=wap;tunnelauthpassword=wap";
     public static final String ADS_URL = "http://www.statusforsale.net/MANUAL_EXECUTION/mobilead.php";
 }
