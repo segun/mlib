e2ecd1894d7373b2f47fb359d76bdf2ff8483532
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2011-04-27 11:10:06 +0100

    changes to URLConstants to enable sparation of bblib

--- src/com/trinisoft/mlib/util/URLConstants.java
+++ src/com/trinisoft/mlib/util/URLConstants.java
@@ -10,12 +10,14 @@ package com.trinisoft.mlib.util;
  */
 public class URLConstants {
     public static String platform = System.getProperty("microedition.platform");
-    
-    public static String MTN_APN = ";apn=web.gprs.mtnnigeria.net;"
-            + "tunnelauthusername=web;tunnelauthpassword=web";
-    public static String ZAIN_APN = ";apn=internet.ng.zain.com;"
-            + "tunnelauthusername=web;tunnelauthpassword=web";
-    public static String GLO_APN = ";apn=glowap;"
-            + "tunnelauthusername=wap;tunnelauthpassword=wap";
+
+    public static String getConnectionParams() {
+        return "";
+    }
+
+    public static String getAPN(String not_used) {
+        return "";
+    }
+
     public static final String ADS_URL = "http://www.statusforsale.net/MANUAL_EXECUTION/mobilead.php";
 }
