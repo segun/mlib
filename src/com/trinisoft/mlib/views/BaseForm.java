df802fd1a840552b543cf76c8c3ac5268fd60473
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2011-05-06 16:50:57 +0100

    No Base Dialog

--- src/com/trinisoft/mlib/views/BaseForm.java
+++ src/com/trinisoft/mlib/views/BaseForm.java
@@ -4,16 +4,12 @@
  */
 package com.trinisoft.mlib.views;
 
-import com.sun.lwuit.Button;
-import com.sun.lwuit.Command;
+import com.sun.lwuit.Dialog;
 import com.sun.lwuit.Form;
 import com.sun.lwuit.Image;
 import com.sun.lwuit.Label;
 import com.sun.lwuit.animations.CommonTransitions;
-import com.sun.lwuit.events.ActionEvent;
-import com.sun.lwuit.events.ActionListener;
 import com.sun.lwuit.layouts.GridLayout;
-import com.sun.lwuit.plaf.Border;
 import javax.microedition.midlet.MIDlet;
 
 /**
@@ -23,7 +19,7 @@ import javax.microedition.midlet.MIDlet;
 public class BaseForm extends Form {
 
     MIDlet midlet;
-    BaseDialog errorDialog, loginDialog, messageDialog;    
+    //BaseDialog errorDialog, loginDialog, messageDialog;    
     Image advertImage;
     boolean isRunning = false;
     public CommonTransitions in, out;
@@ -37,46 +33,52 @@ public class BaseForm extends Form {
         setTransitionInAnimator(in);
     }
 
-    public BaseDialog getMessageDialog(String message, boolean display) {
-        messageDialog = new BaseDialog("MESSAGE");
-        messageDialog.getStyle().setBorder(Border.createLineBorder(5, 0x00faaf));
-        messageDialog.addComponent(new Button(new Command(message)));
-        Command okCommand = new Command("OK");
-        messageDialog.setBackCommand(okCommand);
-        messageDialog.addCommand(okCommand);                
-        messageDialog.addCommandListener(new ActionListener() {
+    public BaseForm() {
+        super("");
+    }
+    
+    
 
-            public void actionPerformed(ActionEvent ae) {
-                messageDialog.dispose();
-            }
-        });
-        messageDialog.setTransitionInAnimator(in);
-        if (display) {
-            messageDialog.showDialog();
-        }
-        return messageDialog;
+    public boolean showMessageDialog(String title, String message, boolean display) {
+        return Dialog.show(title, message, "OK", "Cancel");
+//        messageDialog = new BaseDialog("MESSAGE");
+//        messageDialog.getStyle().setBorder(Border.createLineBorder(5, 0x00faaf));
+//        messageDialog.addComponent(new Button(new Command(message)));
+//        Command okCommand = new Command("OK");
+//        messageDialog.setBackCommand(okCommand);
+//        messageDialog.addCommand(okCommand);                
+//        messageDialog.addCommandListener(new ActionListener() {
+//
+//            public void actionPerformed(ActionEvent ae) {
+//                messageDialog.dispose();
+//            }
+//        });
+//        messageDialog.setTransitionInAnimator(in);
+//        if (display) {
+//            messageDialog.showDialog();
+//        }
+//        return messageDialog;
     }
 
-    public BaseDialog getErrorDialog(String error, boolean display) {
-        errorDialog = new BaseDialog("ERROR");
-        errorDialog.getStyle().setBorder(Border.createLineBorder(5, 0xff0000));
-        errorDialog.addComponent(new Button(new Command(error)));
-        Command okCommand = new Command("OK");
-        errorDialog.setBackCommand(okCommand);
-        errorDialog.addCommand(okCommand);
-        errorDialog.addCommandListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent ae) {
-                errorDialog.dispose();
-            }
-        });
-        errorDialog.setTransitionInAnimator(in);
-        if (display) {
-            errorDialog.showDialog();
-        }        
-        return errorDialog;
-    }
-
+//    public BaseDialog getErrorDialog(String error, boolean display) {
+//        errorDialog = new BaseDialog("ERROR");
+//        errorDialog.getStyle().setBorder(Border.createLineBorder(5, 0xff0000));
+//        errorDialog.addComponent(new Button(new Command(error)));
+//        Command okCommand = new Command("OK");
+//        errorDialog.setBackCommand(okCommand);
+//        errorDialog.addCommand(okCommand);
+//        errorDialog.addCommandListener(new ActionListener() {
+//
+//            public void actionPerformed(ActionEvent ae) {
+//                errorDialog.dispose();
+//            }
+//        });
+//        errorDialog.setTransitionInAnimator(in);
+//        if (display) {
+//            errorDialog.showDialog();
+//        }        
+//        return errorDialog;
+//    }
     protected void addLabel(String label) {
         addComponent(new Label(label));
     }    
