651699137e7345146ef4f631dc14b7e03664319f
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2011-05-09 16:47:15 +0100

    showPleaseWaitDialog()

--- src/com/trinisoft/mlib/util/URLConstants.java
+++ src/com/trinisoft/mlib/util/URLConstants.java
@@ -9,6 +9,8 @@ package com.trinisoft.mlib.util;
  * @author trinisoftinc
  */
 public class URLConstants extends com.trinisoft.baselib.util.URLConstants {
+    
+    public static final String BASE_NEW_VERSION_URL = BASE_URL + "version.php?action=GET_VERSION_URL&model=NONE";    
     public static String platform = System.getProperty("microedition.platform");
 
     public static String getConnectionParams() {
--- src/com/trinisoft/mlib/views/BaseForm.java
+++ src/com/trinisoft/mlib/views/BaseForm.java
@@ -4,12 +4,18 @@
  */
 package com.trinisoft.mlib.views;
 
+import com.sun.lwuit.Button;
+import com.sun.lwuit.Command;
 import com.sun.lwuit.Dialog;
 import com.sun.lwuit.Form;
 import com.sun.lwuit.Image;
 import com.sun.lwuit.Label;
+import com.sun.lwuit.TextArea;
 import com.sun.lwuit.animations.CommonTransitions;
+import com.sun.lwuit.events.ActionEvent;
+import com.sun.lwuit.events.ActionListener;
 import com.sun.lwuit.layouts.GridLayout;
+import com.sun.lwuit.util.Resources;
 import javax.microedition.midlet.MIDlet;
 
 /**
@@ -41,45 +47,35 @@ public class BaseForm extends Form {
 
     public boolean showMessageDialog(String title, String message, boolean display) {
         return Dialog.show(title, message, "OK", "Cancel");
-//        messageDialog = new BaseDialog("MESSAGE");
-//        messageDialog.getStyle().setBorder(Border.createLineBorder(5, 0x00faaf));
-//        messageDialog.addComponent(new Button(new Command(message)));
-//        Command okCommand = new Command("OK");
-//        messageDialog.setBackCommand(okCommand);
-//        messageDialog.addCommand(okCommand);                
-//        messageDialog.addCommandListener(new ActionListener() {
-//
-//            public void actionPerformed(ActionEvent ae) {
-//                messageDialog.dispose();
-//            }
-//        });
-//        messageDialog.setTransitionInAnimator(in);
-//        if (display) {
-//            messageDialog.showDialog();
-//        }
 //        return messageDialog;
     }
 
-//    public BaseDialog getErrorDialog(String error, boolean display) {
-//        errorDialog = new BaseDialog("ERROR");
-//        errorDialog.getStyle().setBorder(Border.createLineBorder(5, 0xff0000));
-//        errorDialog.addComponent(new Button(new Command(error)));
-//        Command okCommand = new Command("OK");
-//        errorDialog.setBackCommand(okCommand);
-//        errorDialog.addCommand(okCommand);
-//        errorDialog.addCommandListener(new ActionListener() {
-//
-//            public void actionPerformed(ActionEvent ae) {
-//                errorDialog.dispose();
-//            }
-//        });
-//        errorDialog.setTransitionInAnimator(in);
-//        if (display) {
-//            errorDialog.showDialog();
-//        }        
-//        return errorDialog;
-//    }
     protected void addLabel(String label) {
         addComponent(new Label(label));
     }    
+    
+    public Dialog showPleaseWait(String pleaseWaitTitle, String pleaseWaitText, Resources r, String loadingID) {
+        //Dialog.show(pleaseWaitTitle, pleaseWaitText, Dialog.TYPE_INFO, r.getImage(loadingID), "OK", "Cancel") ;
+        Dialog d = new Dialog(pleaseWaitTitle);
+        d.setTransitionInAnimator(in);
+        //d.setLayout(new GridLayout(10, 1));
+        TextArea text = new TextArea(pleaseWaitText);
+        text.setEditable(false);
+        Label l = new Label();
+        l.setIcon(r.getImage(loadingID));
+        l.setAlignment(Label.CENTER);
+        d.addComponent(text);
+        d.addComponent(l);
+        Command c = new Command("OK");
+        d.addComponent(new Button(c));
+        d.setBackCommand(c);
+
+        d.addCommandListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+            }
+        });
+        d.showModeless();
+        return d;
+    }
 }
