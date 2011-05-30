3e1f425c2b5e827ae9a14daf6e72a7cf278bc43c
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2011-05-30 19:09:49 +0100

    Notify on installs

--- src/com/trinisoft/mlib/util/URLConstants.java
+++ src/com/trinisoft/mlib/util/URLConstants.java
@@ -4,6 +4,9 @@
  */
 package com.trinisoft.mlib.util;
 
+import com.trinisoft.baselib.io.HttpPull;
+import javax.microedition.rms.RecordStore;
+
 /**
  *
  * @author trinisoftinc
@@ -19,5 +22,25 @@ public class URLConstants extends com.trinisoft.baselib.util.URLConstants {
 
     public static String getAPN(String not_used) {
         return "";
+    }
+    
+    public static void doFirstTime(final String name) throws Exception {
+        final RecordStore rs = RecordStore.openRecordStore("FTRS", true);
+        if (rs.getNumRecords() > 0) {
+        } else {
+            new Thread() {
+
+                public void run() {
+                    HttpPull pull = new HttpPull();
+                    try {
+                        pull.get("http://nerdlines.com/auto/install_notify.php?product_name=" + name + "&action=1", getAPN(""));
+                        byte[] data = "true".getBytes();
+                        rs.addRecord(data, 0, data.length);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }.start();
+        }
     }
 }
